a0bd638375e6ee4e5410865e52605d8d78247aa8
De-veloper <tewen.chiu@gmail.com>
2017-12-16 10:52:21 -0800

    simple funtion

--- fabfile.py
+++ fabfile.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python
 from fabric.api import *
-
 def hello():
     print("Hello World!")
 
 env.use_ssh_config = False
-env.hosts = ["10.0.0.20"]
-env.user = "root"
+env.hosts = ["localhost"]
+#env.user = ""
 #env.key_filename = "/root/.ssh/id_rsa"
-env.password = "Password!"
-env.port = 22
+env.password = ""
+#env.port = 22
  
  
 def uptime():
-        run("uptime")
-        run ("ls -ltr")
+    run("uptime")
+    run ("ls")
